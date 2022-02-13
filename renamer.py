19560c763dd09dfb192a134ecf0cbb795a4610f5
Billthedeve <88978207+Billthedeve@users.noreply.github.com>
2022-02-13 15:41:49 +0200

    ren

--- renamer.py
+++ renamer.py
@@ -2,10 +2,10 @@ import os
 
 fpath = input("Enter the directory the file is in... ")
 
-if "C:/" not in fpath:
+if "C:\\" not in fpath:
     print("invalid directory. File must be in C:/")
 
-if "C:/" in fpath:
+if "C:\\" in fpath:
     os.system("cd " + fpath)
     name = input("Enter file name...")
     newname = input("Enter new file name...")
